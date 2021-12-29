497f326caeb95a5b9517da40a41736623d3017ea
imvivek135 <vbpaladiya02@gmail.com>
2021-12-29 16:38:54 +0530

    Initial Commit

---
+++ dashboard/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from dashboard.models import Notes, Homework, Todo
+
+admin.site.register(Notes)
+admin.site.register(Homework)
+admin.site.register(Todo)
+
+
+# Register your models here.
